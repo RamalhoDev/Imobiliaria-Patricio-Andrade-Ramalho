c0adc8b986ea2573e2950bd354c40e0eb157fec0
Gabrieltp087 <32223762+Gabrieltp087@users.noreply.github.com>
2018-03-21 14:19:41 -0300

    Update estrutura.c

--- estrutura.c
+++ estrutura.c
@@ -23,15 +23,15 @@ void SubMenu(){
             Cadastrar_Imóveis();
             break;
         case 2:
-            printf("2-1.Consultar imóveis por tipo (casa, apartamento ou terreno:\n");
-            printf("2-2.Consultar imóveis para vender por bairro:\n");
-            printf("2-3.Consultar imóveis para alugar por bairro:\n");
-            printf("2-4.Consultar imóveis disponíveis por cidade:\n", );
-            printf("2-5.Consultar todos os imóveis :\n");
-            printf("2-6.Consultar todos  os imóveis e suas características:\n");
+            printf("2-1.Consultar imóveis por tipo (casa, apartamento ou terreno).\n");
+            printf("2-2.Consultar imóveis para vender.\n");
+            printf("2-3.Consultar imóveis para alugar.\n");
+            printf("2-4.Consultar imóveis disponíveis por cidade.\n", );
+            printf("2-5.Consultar todos os imóveis.\n");
+            printf("2-6.Consultar todos  os imóveis e suas características.\n");
             printf("Digite sua opção (1 a 6):  ");
             scanf("%d", &escolha);
-
+            return escolha;
             break;
     }
 
